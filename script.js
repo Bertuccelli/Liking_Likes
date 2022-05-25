6040def657c88bf463089d5952c6e6f8ae456860
Amanda Bertuccelli <akbertuccelli@gmail.com>
2022-05-24 23:01:34 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function counter() {
+    document.querySelector("#counting").innerText++;
+}
+function counter2() {
+    document.querySelector("#counting2").innerText++;
+}
+function counter3() {
+    document.querySelector("#counting3").innerText++;
+}
